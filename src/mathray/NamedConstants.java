14c369f6e0dd77e0ee326fc4579e60fa5e82005a
Joshua Warner <joshua.warner@readytalk.com>
2011-10-19 11:44:21 -0600

    be explicit about the types of named constants

--- src/mathray/NamedConstants.java
+++ src/mathray/NamedConstants.java
@@ -6,12 +6,12 @@ public final class NamedConstants {
   
   private NamedConstants() {}
   
-  public static final Value PI = var("pi");
+  public static final Variable PI = var("pi");
   
-  public static final Value E = var("e");
+  public static final Variable E = var("e");
   
-  public static final Value POS_INF = var("pos_inf");
+  public static final Variable POS_INF = var("pos_inf");
   
-  public static final Value NEG_INF = var("neg_inf");
+  public static final Variable NEG_INF = var("neg_inf");
 
 }
