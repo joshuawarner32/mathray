6f64ac1a427f78f1cb25b690e467b45ccc896b59
Joshua Warner <joshuawarner32@gmail.com>
2011-11-02 21:46:14 -0600

    work on domains

--- src/main/java/mathray/domain/MachineNode.java
+++ src/main/java/mathray/domain/MachineNode.java
@@ -1,27 +1,10 @@
 package mathray.domain;
 
-class MachineNode implements Node {
-  
-  private Node parent;
-  
-  private double[] lowLimits;
-  private double[] highLimits;
-  
-  private int splitAxis;
-  
-  private Node child1;
-  private Node child2;
+class MachineNode extends Node {
 
   @Override
-  public void split(int axis) {
-    // TODO Auto-generated method stub
-    
-  }
-
-  @Override
-  public Node extend(int axis) {
-    // TODO Auto-generated method stub
-    return null;
+  protected Node child() {
+    return new MachineNode();
   }
 
 }
--- src/main/java/mathray/domain/Node.java
+++ src/main/java/mathray/domain/Node.java
@@ -1,8 +1,46 @@
 package mathray.domain;
 
-public interface Node {
-  // negative axis for the negative direction
-  public void split(int axis);
+public abstract class Node {
   
-  public Node extend(int axis);
+  private Node parent;
+  
+  private double[] lowLimits;
+  private double[] highLimits;
+  
+  private int splitAxis;
+  
+  private Node child1;
+  private Node child2;
+  
+  private boolean maybeComplex;
+  private boolean definitelyComplex;
+  
+  // negative axis for the negative direction
+  public void split(int axis) {
+    if(child1 != null) {
+      throw new IllegalStateException("node already split");
+    }
+    splitAxis = axis;
+    child1 = makeChild();
+    child2 = makeChild();
+  }
+  
+  private Node makeChild() {
+    Node ret = child();
+    ret.definitelyComplex = definitelyComplex;
+    ret.maybeComplex = (definitelyComplex || maybeComplex) && !ret.definitelyNotComplex();
+    ret.parent = this;
+    return ret;
+  }
+  
+  private boolean definitelyNotComplex() {
+    // TODO: use inclusion arithmetic to compute whether complex values can ever crop up.
+    return false;
+  }
+  
+  protected abstract Node child();
+  
+  public Node extend(int axis) {
+    return null;
+  }
 }
