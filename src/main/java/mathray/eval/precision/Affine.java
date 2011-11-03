4ef08ff07dbcdd8d8db454e9e85f7eaac56d06c4
Joshua Warner <joshuawarner32@gmail.com>
2011-11-03 07:45:11 -0600

    (mostly) implement Affine.affine

--- src/main/java/mathray/eval/precision/Affine.java
+++ src/main/java/mathray/eval/precision/Affine.java
@@ -1,16 +1,27 @@
 package mathray.eval.precision;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 import mathray.Args;
 import mathray.Definition;
+import mathray.Rational;
+import mathray.Value;
+import mathray.Variable;
 import mathray.Vector;
+import mathray.eval.Binder;
+import mathray.eval.Context;
 import mathray.eval.Environment;
 import mathray.eval.Impl;
+import mathray.eval.Translator;
+import mathray.eval.precision.AffineContext.AffineTerm;
 
 import static mathray.Expressions.*;
 import static mathray.Functions.*;
 
 public class Affine {
-  Environment<AffineForm> env = Environment.<AffineForm>builder()
+  private static Environment<AffineForm> env = Environment.<AffineForm>builder()
     .register(ADD, new Impl<AffineForm>() {
       @Override
       public Vector<AffineForm> call(Vector<AffineForm> args) {
@@ -20,7 +31,20 @@ public class Affine {
     .build();
   
   public static Vector<AffineForm> affine(Definition def, Args args) {
-    return null;
+    AffineContext affineContext = new AffineContext();
+    return new Context<AffineForm>(new Binder<AffineForm>() {
+      @Override
+      public AffineForm bind(Variable var) {
+        Map<AffineTerm, Value> map = new HashMap<AffineTerm, Value>();
+        // TODO: add variance term
+        return new AffineForm(var, map);
+      }
+    }, env, new Translator<AffineForm>() {
+      @Override
+      public AffineForm translate(Rational r) {
+        return new AffineForm(r, new HashMap<AffineTerm, Value>());
+      }
+    }).run(def.values);
   }
 
 }
