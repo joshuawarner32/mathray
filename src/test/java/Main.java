1a2dca95b9034372987325f01a7c7f0e07ea9531
Joshua Warner <joshua.warner@readytalk.com>
2011-12-12 12:27:54 -0700

    reenable printing simplified expressions

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -2,6 +2,8 @@ import mathray.Args;
 import mathray.Definition;
 import mathray.NamedConstants;
 import mathray.Symbol;
+import mathray.eval.simplify.Simplifications;
+import mathray.eval.text.DefaultPrinter;
 import mathray.random.ValueRandom;
 import static mathray.Expressions.*;
 import static mathray.Functions.*;
@@ -18,10 +20,10 @@ public class Main {
     Args a = args(x);
     do {
       Definition orig = random.randomDefinition(a, 0.99, 0.5, 0.7);
-      //Definition simple = Simplifications.simplify(orig);
+      Definition simple = Simplifications.simplify(orig);
       System.out.println(orig.toJavaString());
-      //System.out.println("\ttraditional: " + DefaultPrinter.toString(orig));
-      //System.out.println("\tsimplified:  " + DefaultPrinter.toString(simple));
+      System.out.println("\ttraditional: " + DefaultPrinter.toString(orig));
+      System.out.println("\tsimplified:  " + DefaultPrinter.toString(simple));
     } while(true);//while(!System.console().readLine().equals("q"));
   }
 }
