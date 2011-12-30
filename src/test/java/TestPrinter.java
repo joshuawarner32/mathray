7fd4d743a088383d1418f2f0c0990ea16b9fb940
Joshua Warner <joshuawarner32@gmail.com>
2011-12-29 20:31:27 -0700

    Use DefaultPrinter in TestPrinter

--- src/test/java/TestPrinter.java
+++ src/test/java/TestPrinter.java
@@ -1,67 +1,60 @@
 import static mathray.Expressions.*;
-import static mathray.Functions.*;
 import static org.junit.Assert.*;
 import mathray.NamedConstants;
 import mathray.Symbol;
-import mathray.eval.text.InfixOperator.Associativity;
-import mathray.eval.text.ParseInfo;
+import mathray.Value;
 import org.junit.Test;
 
 public class TestPrinter {
   
   Symbol x = sym("x");
   Symbol y = sym("y");
-  ParseInfo parser = ParseInfo.builder()
-    .group("(", ",", ")")
-    .infix("+", 10, Associativity.LEFT, ADD)
-    .infix("-", 10, Associativity.LEFT, SUB)
-    .infix("*", 20, Associativity.LEFT, MUL)
-    .infix("/", 20, Associativity.LEFT, DIV)
-    .infix("^", 30, Associativity.RIGHT, POW)
-    .prefix("-", 25, NEG)
-    .build();
+  
+  private static void assertPrintsTo(Value val, String str) {
+    assertEquals(str, val.toString());
+  }
   
   @Test
   public void testVariables() {
-    assertEquals("x", parser.unparse(x));
-    assertEquals("tau", parser.unparse(NamedConstants.TAU));
+    assertPrintsTo(x, "x");
+    assertPrintsTo(NamedConstants.TAU, "tau");
   }
   
   @Test
   public void testRationals() {
-    assertEquals("0", parser.unparse(num(0)));
-    assertEquals("1", parser.unparse(num(1)));
-    assertEquals("0", parser.unparse(num(0, 1)));
-    assertEquals("1/2", parser.unparse(num(1, 2)));
+    assertPrintsTo(num(0), "0");
+    assertPrintsTo(num(1), "1");
+    assertPrintsTo(num(0, 1), "0");
+    assertPrintsTo(num(1, 2), "1/2");
   }
   
   @Test
   public void testOperators() {
-    assertEquals("x+y", parser.unparse(add(x, y)));
-    assertEquals("x-y", parser.unparse(sub(x, y)));
-    assertEquals("x*y", parser.unparse(mul(x, y)));
-    assertEquals("x/y", parser.unparse(div(x, y)));
-    assertEquals("x^y", parser.unparse(pow(x, y)));
+    assertPrintsTo(add(x, y), "x+y");
+    assertPrintsTo(sub(x, y), "x-y");
+    assertPrintsTo(mul(x, y), "x*y");
+    assertPrintsTo(div(x, y), "x/y");
+    assertPrintsTo(pow(x, y), "x^y");
   }
   
   @Test
   public void testOrderOfOps() {
-    assertEquals("1+2*3", parser.unparse(add(num(1), mul(num(2), num(3)))));
-    assertEquals("1*(2+3)", parser.unparse(mul(num(1), add(num(2), num(3)))));
+    assertPrintsTo(add(num(1), mul(num(2), num(3))), "1+2*3");
+    assertPrintsTo(mul(num(1), add(num(2), num(3))), "1*(2+3)");
   }
   
   @Test
   public void testAssociativity() {
-    assertEquals("1+2+3", parser.unparse(add(add(num(1), num(2)), num(3))));
-    assertEquals("1+(2+3)", parser.unparse(add(num(1), add(num(2), num(3)))));
-    assertEquals("(1^2)^3", parser.unparse(pow(pow(num(1), num(2)), num(3))));
-    assertEquals("1^2^3", parser.unparse(pow(num(1), pow(num(2), num(3)))));
+    assertPrintsTo(add(add(num(1), num(2)), num(3)), "1+2+3");
+    assertPrintsTo(add(num(1), add(num(2), num(3))), "1+(2+3)");
+    assertPrintsTo(pow(pow(num(1), num(2)), num(3)), "(1^2)^3");
+    assertPrintsTo(pow(num(1), pow(num(2), num(3))), "1^2^3");
   }
   
   @Test
   public void testPrefix() {
-    assertEquals("-x", parser.unparse(neg(x)));
-    assertEquals("-1", parser.unparse(neg(num(1))));
+    assertPrintsTo(neg(x), "-x");
+    assertPrintsTo(neg(num(1)), "-1");
   }
 
 }
