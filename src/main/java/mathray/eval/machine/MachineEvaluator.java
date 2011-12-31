9cca5e2dd4867f8ab209d0cd7f4d543adcdb93f5
Joshua Warner <joshuawarner32@gmail.com>
2011-12-31 10:57:27 -0700

    move MachineEvaluator to FunctionSymbolRegistrar, delete now unused EvalData

--- src/main/java/mathray/eval/machine/MachineEvaluator.java
+++ src/main/java/mathray/eval/machine/MachineEvaluator.java
@@ -3,153 +3,158 @@ package mathray.eval.machine;
 import mathray.Call;
 import mathray.Computation;
 import mathray.Definition;
+import mathray.FunctionSymbolRegistrar;
 import mathray.Rational;
 import mathray.Symbol;
-import mathray.eval.EvalData;
 import mathray.eval.Impl;
 import mathray.util.Vector;
 import mathray.visitor.EvaluatingVisitor;
 
 import static mathray.Functions.*;
 
-public class MachineEvaluator {
+public class MachineEvaluator extends FunctionSymbolRegistrar<Impl<Double>, Double> {
   
-  private static final EvalData<Impl<Double>, Double> env =
-      EvalData.<Impl<Double>, Double>builder()
-      .register(ADD, new Impl<Double>() {
-        public Double call(Vector<Double> args) {
-          return args.get(0) + args.get(1);
-        }
-      })
-      .register(SUB, new Impl<Double>() {
-        public Double call(Vector<Double> args) {
-          return args.get(0) - args.get(1);
-        }
-      })
-      .register(MUL, new Impl<Double>() {
-        public Double call(Vector<Double> args) {
-          return args.get(0) * args.get(1);
-        }
-      })
-      .register(DIV, new Impl<Double>() {
-        public Double call(Vector<Double> args) {
-          return args.get(0) / args.get(1);
-        }
-      })
-      .register(NEG, new Impl<Double>() {
-        public Double call(Vector<Double> args) {
-          return -args.get(0);
-        }
-      })
-      .register(POW, new Impl<Double>() {
-        public Double call(Vector<Double> args) {
-          return Math.pow(args.get(0), args.get(1));
-        }
-      })
-      .register(SIN, new Impl<Double>() {
-        public Double call(Vector<Double> args) {
-          return Math.sin(args.get(0));
-        }
-      })
-      .register(SINH, new Impl<Double>() {
-        public Double call(Vector<Double> args) {
-          return Math.sinh(args.get(0));
-        }
-      })
-      .register(ASIN, new Impl<Double>() {
-        public Double call(Vector<Double> args) {
-          return Math.asin(args.get(0));
-        }
-      })
-      .register(COS, new Impl<Double>() {
-        public Double call(Vector<Double> args) {
-          return Math.cos(args.get(0));
-        }
-      })
-      .register(COSH, new Impl<Double>() {
-        public Double call(Vector<Double> args) {
-          return Math.cosh(args.get(0));
-        }
-      })
-      .register(ACOS, new Impl<Double>() {
-        public Double call(Vector<Double> args) {
-          return Math.acos(args.get(0));
-        }
-      })
-      .register(TAN, new Impl<Double>() {
-        public Double call(Vector<Double> args) {
-          return Math.tan(args.get(0));
-        }
-      })
-      .register(TANH, new Impl<Double>() {
-        public Double call(Vector<Double> args) {
-          return Math.tanh(args.get(0));
-        }
-      })
-      .register(ATAN, new Impl<Double>() {
-        public Double call(Vector<Double> args) {
-          return Math.atan(args.get(0));
-        }
-      })
-      .register(ATAN2, new Impl<Double>() {
-        public Double call(Vector<Double> args) {
-          return Math.atan2(args.get(0), args.get(1));
-        }
-      })
-      .register(LOG, new Impl<Double>() {
-        public Double call(Vector<Double> args) {
-          return Math.log(args.get(0));
-        }
-      })
-      .register(SQRT, new Impl<Double>() {
-        public Double call(Vector<Double> args) {
-          return Math.sqrt(args.get(0));
-        }
-      })
-      .register(MIN, new Impl<Double>() {
-        public Double call(Vector<Double> args) {
-          return Math.min(args.get(0), args.get(1));
-        }
-      })
-      .register(MAX, new Impl<Double>() {
-        public Double call(Vector<Double> args) {
-          return Math.max(args.get(0), args.get(1));
-        }
-      })
-      .register(ABS, new Impl<Double>() {
-        public Double call(Vector<Double> args) {
-          return Math.abs(args.get(0));
-        }
-      })
-      .register(SELECT_SIGN, new Impl<Double>() {
-        public Double call(Vector<Double> args) {
-          double test = args.get(0);
-          if(test < 0) {
-            return args.get(1);
-          } else if(test > 0) {
-            return args.get(3);
-          } else {
-            return args.get(2);
-          }
-        }
-      })
-      .register(UP, new Impl<Double>() {
-        public Double call(Vector<Double> args) {
-          return Math.nextUp(args.get(0));
-        }
-      })
-      .register(DOWN, new Impl<Double>() {
-        public Double call(Vector<Double> args) {
-          return -Math.nextUp(-args.get(0));
-        }
-      })
-      .build();
+  private static final MachineEvaluator INSTANCE = new MachineEvaluator();
   
+  {
+    register(ADD, new Impl<Double>() {
+      public Double call(Vector<Double> args) {
+        return args.get(0) + args.get(1);
+      }
+    });
+    register(SUB, new Impl<Double>() {
+      public Double call(Vector<Double> args) {
+        return args.get(0) - args.get(1);
+      }
+    });
+    register(MUL, new Impl<Double>() {
+      public Double call(Vector<Double> args) {
+        return args.get(0) * args.get(1);
+      }
+    });
+    register(DIV, new Impl<Double>() {
+      public Double call(Vector<Double> args) {
+        return args.get(0) / args.get(1);
+      }
+    });
+    register(NEG, new Impl<Double>() {
+      public Double call(Vector<Double> args) {
+        return -args.get(0);
+      }
+    });
+    register(POW, new Impl<Double>() {
+      public Double call(Vector<Double> args) {
+        return Math.pow(args.get(0), args.get(1));
+      }
+    });
+    register(SIN, new Impl<Double>() {
+      public Double call(Vector<Double> args) {
+        return Math.sin(args.get(0));
+      }
+    });
+    register(SINH, new Impl<Double>() {
+      public Double call(Vector<Double> args) {
+        return Math.sinh(args.get(0));
+      }
+    });
+    register(ASIN, new Impl<Double>() {
+      public Double call(Vector<Double> args) {
+        return Math.asin(args.get(0));
+      }
+    });
+    register(COS, new Impl<Double>() {
+      public Double call(Vector<Double> args) {
+        return Math.cos(args.get(0));
+      }
+    });
+    register(COSH, new Impl<Double>() {
+      public Double call(Vector<Double> args) {
+        return Math.cosh(args.get(0));
+      }
+    });
+    register(ACOS, new Impl<Double>() {
+      public Double call(Vector<Double> args) {
+        return Math.acos(args.get(0));
+      }
+    });
+    register(TAN, new Impl<Double>() {
+      public Double call(Vector<Double> args) {
+        return Math.tan(args.get(0));
+      }
+    });
+    register(TANH, new Impl<Double>() {
+      public Double call(Vector<Double> args) {
+        return Math.tanh(args.get(0));
+      }
+    });
+    register(ATAN, new Impl<Double>() {
+      public Double call(Vector<Double> args) {
+        return Math.atan(args.get(0));
+      }
+    });
+    register(ATAN2, new Impl<Double>() {
+      public Double call(Vector<Double> args) {
+        return Math.atan2(args.get(0), args.get(1));
+      }
+    });
+    register(LOG, new Impl<Double>() {
+      public Double call(Vector<Double> args) {
+        return Math.log(args.get(0));
+      }
+    });
+    register(SQRT, new Impl<Double>() {
+      public Double call(Vector<Double> args) {
+        return Math.sqrt(args.get(0));
+      }
+    });
+    register(MIN, new Impl<Double>() {
+      public Double call(Vector<Double> args) {
+        return Math.min(args.get(0), args.get(1));
+      }
+    });
+    register(MAX, new Impl<Double>() {
+      public Double call(Vector<Double> args) {
+        return Math.max(args.get(0), args.get(1));
+      }
+    });
+    register(ABS, new Impl<Double>() {
+      public Double call(Vector<Double> args) {
+        return Math.abs(args.get(0));
+      }
+    });
+    register(SELECT_SIGN, new Impl<Double>() {
+      public Double call(Vector<Double> args) {
+        double test = args.get(0);
+        if(test < 0) {
+          return args.get(1);
+        } else if(test > 0) {
+          return args.get(3);
+        } else {
+          return args.get(2);
+        }
+      }
+    });
+    register(UP, new Impl<Double>() {
+      public Double call(Vector<Double> args) {
+        return Math.nextUp(args.get(0));
+      }
+    });
+    register(DOWN, new Impl<Double>() {
+      public Double call(Vector<Double> args) {
+        return -Math.nextUp(-args.get(0));
+      }
+    });
+  }
+    
   public static Vector<Double> eval(final Computation comp, final Vector<Double> params) {
+    return INSTANCE.transform(comp, params);
+  }
+  
+  public Vector<Double> transform(final Computation comp, final Vector<Double> params) {
     final EvaluatingVisitor<Double> v = new EvaluatingVisitor<Double>() {
       @Override
       public Double call(Call call, Vector<Double> args) {
-        return env.defineFunction(call.func).call(args);
+        return lookup(call.func).call(args);
       }
 
       @Override
@@ -158,7 +163,7 @@ public class MachineEvaluator {
         if(ret != null) {
           return params.get(ret);
         } else {
-          return env.defineSymbol(sym);
+          return lookup(sym);
         }
       }
 
