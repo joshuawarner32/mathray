c9bd80c45878d8c6b7e0e891720a81383dfc571c
Joshua Warner <joshuawarner32@gmail.com>
2012-05-14 06:55:48 -0600

    add (failing) testMultidefEquality (will investigate later)

--- src/test/java/TestEquality.java
+++ src/test/java/TestEquality.java
@@ -28,5 +28,15 @@ public class TestEquality {
   public void testStructHashCode() {
     assertEquals(struct(cos(x)).hashCode(), struct(cos(x)).hashCode());
   }
+  
+  @Test
+  public void testDefinitionEquality() {
+    assertEquals(def(args(x), x), def(args(x), x));
+  }
+  
+  @Test
+  public void testMultidefEquality() {
+    assertEquals(multidef(args(x), x), multidef(args(x), x));
+  }
 
 }
